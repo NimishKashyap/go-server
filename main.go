519031240737612b82de617567e0763bc5fcc504
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-06-12 12:28:43 +0530

    api done

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
 func main() {
+	fileServer := http.FileServer(http.Dir("static"))
+
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error parsing form %v\n", err)
+		return
+	}
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name: %v\n", name)
+	fmt.Fprintf(w, "Address: %v\n", address)
 
 }
